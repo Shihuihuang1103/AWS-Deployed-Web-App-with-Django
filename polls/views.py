0c18cc3316854a06e856d4ac5e3805634c40e3c0
Shihui Huang <sh4480@nyu.edu>
2024-02-29 19:16:18 +0800

    set up coverage & flake8

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
